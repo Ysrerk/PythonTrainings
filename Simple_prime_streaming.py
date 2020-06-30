304888ab086eba80f1eea9f9152081b7ecc4cc38
Yasar ERKAN <ysr.erk2019@gmail.com>
2020-06-30 12:36:09 -0400

    this exercise is about consecutive prime numbers

--- Simple_prime_streaming.py
+++ Simple_prime_streaming.py
@@ -1,3 +1,19 @@
+# This is a questin an explanation
+
+# Consider a sequence made up of the consecutive prime numbers. This infinite sequence would start with:
+#
+# "2357111317192329313741434753596167717379..."
+# You will be given two numbers: a and b, and your task will be to return b elements starting from index a in this sequence.
+#
+# For example:
+# solve(10,5) == `19232` Because these are 5 elements from index 10 in the sequence.
+# Tests go up to about index 20000.
+#
+# More examples in test cases. Good luck!
+
+
+
+
 sayi=int(input("please enter  how many numbers do you want to see prime numbers"))
 nonprime=[]
 prime=[]
